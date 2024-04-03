db9d28e3ab81622f2aaa076ec0b0f9667142e542
gobinath <gobithaagam@gmail.com>
2024-04-03 22:32:29 +0530

    mydashboard add booking

--- mydashboard/urls.py
+++ mydashboard/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='dashboard_login'),
+    path('', views.login, name='mydashboard_login'),
     path('index/', views.index, name='mydashboard_index'),
     path('contact/', views.contact, name='mydashboard_contact'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('Add_booking/', views.Add_booking, name='mydashboard_Add_booking'),
+    path('check_availability/', views.check_availability, name='check_availability'),
+    path('success/', views.success, name='mydashboard_success'),
 
 ]
+
+
+
+
+
+
 
